ae0b46b6188cab7655d1b4262bc07be3bceb2ef0
方亮” <2903680368@qq.com>
2023-03-17 16:20:06 +0800

    <feat> 格式化代码

--- src/views/music/music.vue
+++ src/views/music/music.vue
@@ -1,18 +1,13 @@
 <template>
   <div>
-    <audio
-      ref="audio"
-      src="http://f5.htqyy.com/play9/55/mp3/7"
-      controls
-      autoplay
-      />
+    <el-input v-model="input" placeholder="请输入内容"></el-input>
+    <audio ref="audio" src="http://f5.htqyy.com/play9/55/mp3/7" controls autoplay />
   </div>
 </template>
 
-<script>
-export default {
-  name: 'music'
-}
+<script setup>
+import { ref } from 'vue'
+const input = ref('')
 </script>
 
 <style scoped></style>
