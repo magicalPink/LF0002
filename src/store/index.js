5ce396d7d1f440d0d1bf85c9d312d3f81e94c87c
方亮” <2903680368@qq.com>
2023-03-17 16:19:54 +0800

    <feat> 添加路由缓存功能

--- src/store/index.js
+++ src/store/index.js
@@ -4,17 +4,24 @@ import { getBasicInfo } from '@/api/home'
 const userState = {
   userInfo: null
 }
+const routerState = {
+  cachedViews: []
+}
 
 // Create a new store instance.
 export default createStore({
   state() {
-    return { ...userState }
+    return { ...userState, ...routerState }
   },
   mutations: {
     getUserInfo(state) {
       getBasicInfo().then((res) => {
         state.userInfo = res.data.data
       })
+    },
+    addCachedView(state, name) {
+      if (state.cachedViews.includes(name)) return
+      state.cachedViews.push(name)
     }
   },
   actions: {
