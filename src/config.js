758e9bb672a0cf53f6d49c41ce2e58c4a0f891ec
方亮” <2903680368@qq.com>
2023-07-13 18:31:34 +0800

    <feat> 添加wsUrl

--- src/config.js
+++ src/config.js
@@ -1,6 +1,12 @@
 const baseURL = {
-  development: 'api/',
-  production: 'prod/'
+  //location
+  development: 'http://127.0.0.1:4000/api/',
+  production: 'http://124.222.235.110/prod/'
 }[import.meta.env.MODE]
 
-export default baseURL
+const wsUrl = {
+  development: 'ws://127.0.0.1:3001',
+  production: 'ws://124.222.235.110:3001'
+}[import.meta.env.MODE]
+
+export { baseURL, wsUrl }
