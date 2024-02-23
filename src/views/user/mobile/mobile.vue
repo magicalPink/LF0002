2a60c035f23cb18bdf960d0d445775d2dee58cef
方亮” <2903680368@qq.com>
2024-02-23 14:45:04 +0800

    <feat> 修改图片样式

--- src/views/user/mobile/mobile.vue
+++ src/views/user/mobile/mobile.vue
@@ -2,7 +2,7 @@
   <div class="pt10">
     <div class="userContent mx5 radius5 mb10 p5 flex justify-between">
       <div>
-        <h4>{{param.userStore.userInfo.nickname || 'MeiHL'}}</h4>
+        <h4>{{ param.userStore.userInfo.nickname || "MeiHL" }}</h4>
       </div>
       <van-image
         width="70"
@@ -11,8 +11,8 @@
         radius="5"
       />
     </div>
-    <van-cell title="修改头像" is-link @click="avatarShow = true"/>
-    <van-cell title="修改昵称" is-link @click="nicknameShow = true"/>
+    <van-cell title="修改头像" is-link @click="avatarShow = true" />
+    <van-cell title="修改昵称" is-link @click="nicknameShow = true" />
     <van-cell title="修改密码" is-link />
     <van-cell center title="暗黑主题">
       <template #right-icon>
@@ -50,35 +50,35 @@
       position="right"
       :style="{ width: '100%', height: '100%' }"
     >
-      <van-nav-bar
-        title="修改头像"
-        right-text="确认"
-        left-arrow
-        @click-left="avatarShow = false"
-        @click-right="setAvatar"
-      />
-      <div class="flex justify-center mt10">
-        <van-image
-          width="300"
-          height="300"
-          :src="avatar"
-          radius="10"
-        >
-          <template v-slot:loading>
-            <van-loading type="spinner" size="20" />
-          </template>
-        </van-image>
-      </div>
-      <div class="p10">
-        <van-image
-          class="m5"
-          width="70"
-          height="70"
-          v-for="item in avatarList"
-          :src="item"
-          radius="10"
-          @click="avatar = item"
+      <div class="flex flex-column h100">
+        <van-nav-bar
+          title="修改头像"
+          right-text="确认"
+          left-arrow
+          @click-left="avatarShow = false"
+          @click-right="setAvatar"
         />
+        <div class="flex justify-center my5">
+          <van-image
+            width="80%"
+            :src="avatar"
+            radius="10"
+          >
+            <template v-slot:loading>
+              <van-loading type="spinner" size="20" />
+            </template>
+          </van-image>
+        </div>
+        <div class="auto">
+          <van-image
+            class="ml5 mb5"
+            width="22%"
+            v-for="item in avatarList"
+            :src="item"
+            radius="10"
+            @click="avatar = item"
+          />
+        </div>
       </div>
     </van-popup>
   </div>
@@ -87,59 +87,59 @@
 <script setup>
 import { computed, onMounted, ref } from "vue";
 
-import { inject } from 'vue';
+import { inject } from "vue";
 
 import { useSettingStore } from "@/store/settingStore.js";
 
-const settingsStore = useSettingStore()
+const settingsStore = useSettingStore();
 
-const param = inject('param');
+const param = inject("param");
 
-const nickName = ref('')
+const nickName = ref("");
 
-const theme = computed(() => settingsStore.theme)
+const theme = computed(() => settingsStore.theme);
 
-const nicknameShow = ref(false)
+const nicknameShow = ref(false);
 
-const avatarShow = ref(false)
+const avatarShow = ref(false);
 
-const avatar = ref('')
+const avatar = ref("");
 
 const avatarList = ref([
-  '/image/avatar/avatar1.png',
-  '/image/avatar/avatar2.png',
-  '/image/avatar/avatar3.png',
-  '/image/avatar/avatar4.png',
-  '/image/avatar/avatar5.png',
-  '/image/avatar/avatar6.png',
-  '/image/avatar/avatar7.png',
-  '/image/avatar/avatar8.png',
-  '/image/avatar/avatar9.png',
-  '/image/avatar/avatar10.png',
-])
+  "/image/avatar/avatar1.png",
+  "/image/avatar/avatar2.png",
+  "/image/avatar/avatar3.png",
+  "/image/avatar/avatar4.png",
+  "/image/avatar/avatar5.png",
+  "/image/avatar/avatar6.png",
+  "/image/avatar/avatar7.png",
+  "/image/avatar/avatar8.png",
+  "/image/avatar/avatar9.png",
+  "/image/avatar/avatar10.png"
+]);
 
 const setNickName = () => {
   param.setNickName(
-    {id:param.userStore.userInfo.id, nickname:nickName.value},
+    { id: param.userStore.userInfo.id, nickname: nickName.value },
     () => {
-      nicknameShow.value = false
+      nicknameShow.value = false;
     }
-  )
-}
+  );
+};
 
 const setAvatar = () => {
   param.setUserAvatar(
-    {id:param.userStore.userInfo.id, avatar:avatar.value},
+    { id: param.userStore.userInfo.id, avatar: avatar.value },
     () => {
-      avatarShow.value = false
+      avatarShow.value = false;
     }
-  )
-}
+  );
+};
 
 onMounted(() => {
-  nickName.value = JSON.parse(sessionStorage.getItem("userInfo")).nickname
-  avatar.value = JSON.parse(sessionStorage.getItem("userInfo")).avatar
-})
+  nickName.value = JSON.parse(sessionStorage.getItem("userInfo")).nickname;
+  avatar.value = JSON.parse(sessionStorage.getItem("userInfo")).avatar;
+});
 
 </script>
 
