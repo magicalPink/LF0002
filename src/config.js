944a053d493664b303ce59fda0cc64c75bd2cf45
2903680368 <2903680368@qq.com>
2024-05-31 20:09:58 +0800

    修改域名

--- src/config.js
+++ src/config.js
@@ -2,13 +2,13 @@
 const baseURL = {
   //location
   development: 'http://localhost:4000/api/',
-  production: 'http://111.229.200.218/prod/'
+  production: 'http://meihl.online/prod/'
 }[import.meta.env.MODE]
 
 //基础通信服务
 const wsBaseUrl = {
   development: 'ws://localhost:3001',
-  production: 'ws://111.229.200.218:3001'
+  production: 'ws://meihl.online:3001'
 }[import.meta.env.MODE]
 
 export { baseURL, wsBaseUrl }
