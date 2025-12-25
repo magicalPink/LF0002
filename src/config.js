668a5a42d4889c528c06421b7af5ee2e539fb82f
2903680368 <2903680368@qq.com>
2025-12-25 17:40:38 +0800

    <feat>新增接口

--- src/config.js
+++ src/config.js
@@ -2,13 +2,13 @@
 const baseURL = {
   //location
   development: 'http://localhost:4000/api/',
-  production: 'http://www.meihl.online/prod'
+  production: 'http://111.229.200.218/prod'
 }[import.meta.env.MODE]
 
 //基础通信服务
 const wsBaseUrl = {
   development: 'ws://localhost:3001',
-  production: 'ws://www.meihl.online:3001'
+  production: 'ws://111.229.200.218:3001'
 }[import.meta.env.MODE]
 
 export { baseURL, wsBaseUrl }
